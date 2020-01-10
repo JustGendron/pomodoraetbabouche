3bd83d9a6e15bed23da6588570a8ac36c268eb23
RemsNoel <remy.noel@epsi.fr>
2020-01-10 13:54:50 +0100

    Ajouts commentaires sur Pomodoro.xaml.cs

--- Pomodoro.xaml.cs
+++ Pomodoro.xaml.cs
@@ -11,17 +11,23 @@ namespace pomodoraetbabouche
     /// </summary>
     public partial class Pomodoro : UserControl
     {
+        /// <summary>
+        /// Initiation des variables utilisé dans le programme
+        /// </summary>
         int iteration = 1;
         Boolean workTime = true;
         Boolean finalRestBool = true;
         TimeSpan work = new TimeSpan(0, 0, 0, 5, 0);
         TimeSpan rest = new TimeSpan(0, 0, 0, 2, 0);
         TimeSpan sec = new TimeSpan(0, 0, 0, 1, 0);
-        TimeSpan end = new TimeSpan(0, 0, 0, 0, 0);
         TimeSpan finalRest = new TimeSpan(0, 0, 0, 15, 0);
+        TimeSpan end = new TimeSpan(0, 0, 0, 0, 0);
         DispatcherTimer dispatcherTimer = new DispatcherTimer();
         TimeSpan tempsRestant = new TimeSpan();
 
+        /// <summary>
+        /// Code joué lors de l'initialisation du composant avec le Handler qui permet de faire le décompte
+        /// </summary>
         public Pomodoro()
         {
             InitializeComponent();
@@ -34,14 +40,20 @@ namespace pomodoraetbabouche
 
         }
 
+        /// <summary>
+        /// Methode du déroulé de la session du pomodoro
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
+            // Permet de définir le nombre d'itération de phase de working + rest
             if (iteration < 4)
             {
                 iterationaff.Content = iteration;
                 tempsRestant = tempsRestant.Subtract(sec);
                 ChronoTimer.Content = tempsRestant.ToString(@"mm\:ss");
-
+                // Quand le décompte arrive à 0
                 if (end == tempsRestant)
                 {
                     tempsRestant = new TimeSpan();
@@ -54,6 +66,7 @@ namespace pomodoraetbabouche
                     }
                     else
                     {
+                        //Permet d'éviter de jouer la petite pause avant la grande pause finale
                         if (iteration < 4)
                         {
                             tempsRestant += rest;
@@ -63,13 +76,16 @@ namespace pomodoraetbabouche
                 }
 
             }
+            // Une fois les iterations défini écoulé
             else
             {
                 tempsRestant = tempsRestant.Subtract(sec);
                 ChronoTimer.Content = tempsRestant.ToString(@"mm\:ss");
 
+                // Quand le décompte arrive à zéro
                 if (end == tempsRestant)
                 {
+                    // Permet de passé une fois dans la boucle pour jouer la grande pause
                     if (finalRestBool)
                     {
 
@@ -77,6 +93,7 @@ namespace pomodoraetbabouche
                         tempsRestant += finalRest;
                         finalRestBool = false;
                     }
+                    // Une fois que la grande pause est fini
                     else
                     {
                         dispatcherTimer.Stop();
@@ -88,17 +105,32 @@ namespace pomodoraetbabouche
             CommandManager.InvalidateRequerySuggested();
         }
 
+        /// <summary>
+        /// Methode appelé lors du clic du Bouton Start
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Button_Start(object sender, RoutedEventArgs e)
         {
             dispatcherTimer.Start();
             LabelPomodoro.IsEnabled = false;
         }
 
+        /// <summary>
+        /// Méthode appelé lors du clic du Bouton Pause
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Button_Pause(object sender, RoutedEventArgs e)
         {
             dispatcherTimer.Stop();
         }
 
+        /// <summary>
+        /// Méthode appelé lors du clic du Bouton Stop, déclenche la reinitialisation des variables
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Button_Stop(object sender, RoutedEventArgs e)
         {
             dispatcherTimer.Stop();
@@ -113,14 +145,21 @@ namespace pomodoraetbabouche
             LabelPomodoro.Text = "";
         }
 
+        /// <summary>
+        /// Permet d'obliger la saisie d'un nom pour le pomodoro, pour un enregistrement future dans la BDD
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void LabelPomodoro_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
         {
+            // Si Aucun nom renseigné, alors les boutons sont désactivés
             if (LabelPomodoro.Text.Length == 0)
             {
                 ButtonStart.IsEnabled = false;
                 ButtonPause.IsEnabled = false;
                 ButtonStop.IsEnabled = false;
             }
+            // Si un nom est renseigné, alors les boutons sont activés
             else
             {
                 ButtonStart.IsEnabled = true;
