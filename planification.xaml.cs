02b04dd3cde8f0ee56311aa13d9bd3659ba229c7
JustGendron <37615144+JustGendron@users.noreply.github.com>
2020-02-12 18:33:35 +0100

    planification

---
+++ planification.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pomodoraetbabouche
+{
+    /// <summary>
+    /// Logique d'interaction pour planification.xaml
+    /// </summary>
+    public partial class planification : UserControl
+    {
+        public planification()
+        {
+            InitializeComponent();
+            DateTime localDate = DateTime.Now;
+            selectTag.ItemsSource = typeof(Colors).GetProperties();
+            titre.Content = "Ma journée du " + localDate.ToString("dd/MM/yyyy");
+        }
+
+        private void cmbColors_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Pomodoro p = new Pomodoro(selectTag.SelectedItem.ToString());
+
+        }
+    }
+}
